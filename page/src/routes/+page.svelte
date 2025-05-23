757317dbfdf930871e12dc2444b46f08048d2d76
Jayden <Jaydenhe171@gmail.com>
2025-05-24 01:21:33 +1000

    Update logos with their cropped counterparts, imported the new parallax cards, locally installed the Balatro font and set it up as a font-family. For readability, added a small logo.

--- page/src/routes/+page.svelte
+++ page/src/routes/+page.svelte
@@ -3,10 +3,34 @@
 	import Scene from "./canvas.svelte";
 
 	import logo from "$lib/assets/logo.png";
+	import small_logo from "$lib/assets/small_logo.png";
 
-	import { PlaneGeometry, OrthographicCamera } from "three";
+	let logo_element: HTMLImageElement;
+	let small_logo_element: HTMLImageElement;
+	const parallax_cards_raw = import.meta.glob("$lib/assets/3d_cards/*.png", {
+		eager: true,
+	});
+
+	let parallax_cards = {};
+
+	import { PlaneGeometry } from "three";
+    import { onMount } from "svelte";
 	extend({ PlaneGeometry });
+
 </script>
+
+<style>
+	::-webkit-scrollbar {
+		display: none;
+	}
+
+	@font-face {
+		font-family: balatro;
+		src: url('/fonts/balatro.otf') format('opentype');
+		font-weight: normal;
+	}
+	
+</style>
 
 <div class="no-scrollbar">
 	<div class="w-screen h-screen fixed top-0 left-0 -z-1">
@@ -16,13 +40,15 @@
 			<Scene />
 		</Canvas>
 	</div>
-	<div id="logo" class="flex absolute top-0 left-0 w-screen h-screen -z-1 justify-center items-center">
-		<img src={logo} alt="Logo" class="w-5/8" />
+	<div id="logo" class="flex absolute top-0 left-0 w-screen h-screen -z-1 justify-center items-center flex-col -gap-3">
+		<img src={logo} alt="Logo" class="w-5/8" bind:this={logo_element}/>
+		<!-- translating manually may not accomadate all screen sizes -->
+		<img src={small_logo} alt="Sculio" class="w-1/6 transform-[translate(0,-70px)] " bind:this={small_logo_element}/>
 	</div>
 </div>
 
-<style>
-	::-webkit-scrollbar {
-		display: none;
-	}
-</style>
+{#each Object.entries(parallax_cards_raw) as [key, value]}
+	<div>{key}, {value}</div>
+{/each}
+
+
