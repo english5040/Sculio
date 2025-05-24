fd1794c29ba2ca43aafaa9c3844b6427c30be55d
Jayden <Jaydenhe171@gmail.com>
2025-05-25 02:55:23 +1000

    rebased and fixed merge conflicts

--- page/src/routes/+page.svelte
+++ page/src/routes/+page.svelte
@@ -1,23 +1,63 @@
 <script lang="ts">
-	import { Canvas, extend, T } from "@threlte/core";
+	import { Canvas, extend } from "@threlte/core";
 	import Scene from "./canvas.svelte";
+	import Card_Scene from "./canvas2.svelte"; // Maybe rename canvas2.svelte to something else
 
 	import logo from "$lib/assets/logo.png";
 	import small_logo from "$lib/assets/small_logo.png";
 
 	let logo_element: HTMLImageElement;
 	let small_logo_element: HTMLImageElement;
-	const parallax_cards_raw = import.meta.glob("$lib/assets/3d_cards/*.png", {
-		eager: true,
+
+	import { PlaneGeometry, WebGLRenderer } from "three";
+	import { onMount } from "svelte";
+
+	let card_renderer: WebGLRenderer;
+	onMount(() => {
+		card_renderer = new WebGLRenderer({
+			antialias: true,
+			alpha: true,
+		});
 	});
 
-	let parallax_cards = {};
-
-	import { PlaneGeometry } from "three";
-    import { onMount } from "svelte";
 	extend({ PlaneGeometry });
-
 </script>
+
+<div class="no-scrollbar">
+	<div class="w-screen h-screen fixed top-0 left-0 -z-1">
+		<Canvas dpr={2}>
+			<Scene />
+		</Canvas>
+	</div>
+
+	<div
+		id="logo"
+		class="flex absolute top-0 left-0 w-screen h-screen -z-1 justify-center items-center flex-col -gap-3"
+	>
+		<img
+			src={logo}
+			alt="Logo"
+			class="w-5/8"
+			bind:this={logo_element}
+		/>
+		<!-- translating manually may not accomadate all screen sizes -->
+		<img
+			src={small_logo}
+			alt="Sculio"
+			class="w-1/6 transform-[translate(0,-70px)]"
+			bind:this={small_logo_element}
+		/>
+	</div>
+	<div class="w-screen">
+		<!-- <Canvas dpr={2}>
+			<Card_Scene />
+		</Canvas> -->
+	</div>
+	<!-- add  a scroll area for the canvas to run its animation or whatever -->
+	<div class="w-screen h-1/2 overflow-y-scroll overflow-x-hidden">
+		<div class="h-[200vh]"></div>
+	</div>
+</div>
 
 <style>
 	::-webkit-scrollbar {
@@ -26,29 +66,7 @@
 
 	@font-face {
 		font-family: balatro;
-		src: url('/fonts/balatro.otf') format('opentype');
+		src: url("/fonts/balatro.otf") format("opentype");
 		font-weight: normal;
 	}
-	
 </style>
-
-<div class="no-scrollbar">
-	<div class="w-screen h-screen fixed top-0 left-0 -z-1">
-		<Canvas
-			dpr={2}
-		>
-			<Scene />
-		</Canvas>
-	</div>
-	<div id="logo" class="flex absolute top-0 left-0 w-screen h-screen -z-1 justify-center items-center flex-col -gap-3">
-		<img src={logo} alt="Logo" class="w-1/2" bind:this={logo_element}/>
-		<!-- translating manually may not accomadate all screen sizes -->
-		<img src={small_logo} alt="Sculio" class="w-1/6 transform-[translate(0,-70px)] " bind:this={small_logo_element}/>
-	</div>
-</div>
-
-{#each Object.entries(parallax_cards_raw) as [key, value]}
-	<div>{key}, {value}</div>
-{/each}
-
-
--- page/src/routes/canvas2.svelte
+++ page/src/routes/canvas2.svelte
@@ -1,15 +1,58 @@
 <script lang="ts">
 	import { T, extend } from "@threlte/core";
-	import { PlaneGeometry, MeshBasicMaterial } from "three"; // Import MeshBasicMaterial
+	import { ImageMaterial } from "@threlte/extras";
+	import { PlaneGeometry, DoubleSide } from "three"; // Import MeshBasicMaterial
 	import { Color } from "three"; // Import Color for solid color
 
 	extend({ PlaneGeometry }); // Still need to extend PlaneGeometry
 
-	// We are using MeshBasicMaterial for this test, no need for ShaderMaterial here
-	// No useTask, onMount, uniforms, or iTime/iResolution for this simple test.
+	const parallax_cards_raw = import.meta.glob(
+		"$lib/assets/parallax_cards/*.png",
+		{
+			eager: true,
+		},
+	);
+
+	console.log("parallax_cards_raw", parallax_cards_raw);
+
+	let cards_settings = {
+		"ace.png": {
+			starting_position: [0, 0, 0],
+		},
+		"king.png": {
+			starting_position: [0, 0, 0],
+		},
+		"queen.png": {
+			starting_position: [0, 0, 0],
+		},
+		"jack.png": {
+			starting_position: [0, 0, 0],
+		},
+		"rorschach.png": {
+			starting_position: [0, 0, 0],
+		},
+		"fail.png": {
+			starting_position: [0, 0, 0],
+		},
+		"mad.png": {
+			starting_position: [0, 0, 0],
+		},
+		"pyro.png": {
+			starting_position: [0, 0, 0],
+		},
+	};
 </script>
 
-<T.Mesh>
-	<T.PlaneGeometry args={[2, 2]} />
-	<T.MeshBasicMaterial color={new Color("red")} side={2} />
-</T.Mesh>
+<!-- as you scroll down, the cards will come out from the center of the screen -->
+{#each Object.entries(parallax_cards_raw) as [path, image]}
+	<T.Mesh>
+		<T.PlaneGeometry args={[2, 2]} />
+		<ImageMaterial
+			transparent
+			side={DoubleSide}
+			url={path}
+			radius={0.1}
+			zoom={0.5}
+		/>
+	</T.Mesh>
+{/each}
