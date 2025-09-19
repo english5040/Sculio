79517119118c2bb3cc12dd964d91bb8848ffb78b
english5040 <ejwu@andrew.cmu.edu>
2025-09-19 04:22:39 -0400

    Pop Star: fixes
    `v.debuff == false` may give an incorrect result if `v.debuff` is nil.
    
    Use poll_enhancement. This is good style, but
    also avoids a bug with randomly generating a special enhancement that
    another mod declared with `in_pool() == false`. Most "pools" of items
    in Balatro shouldn't be directly selected from.

--- jokers/7_pop_star.lua
+++ jokers/7_pop_star.lua
@@ -9,15 +9,14 @@ SMODS.Joker {
   pos = { x = 6, y = 0 },
   cost = 7,
   loc_vars = function(self, info_queue, card)
-    return { vars = { (G.GAME.probabilities.normal or 1), card.ability.extra.odds } }
+    local numerator, denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'pop_star')
+    return { vars = { numerator, denominator } }
   end,
   calculate = function(self, card, context)
     if context.before and not context.blueprint then
       for k, v in ipairs(context.scoring_hand) do
-        if v.debuff == false and pseudorandom('pop_star') < G.GAME.probabilities.normal / card.ability.extra.odds then
-          math.randomseed(pseudorandom('pop_star'))
-          enhancement_index = math.random(1, #G.P_CENTER_POOLS['Enhanced'])
-          enhancement = G.P_CENTER_POOLS['Enhanced'][enhancement_index]
+        if not v.debuff and SMODS.pseudorandom_probability(card, 'pop_star', 1, card.ability.extra.odds) then
+          local enhancement = SMODS.poll_enhancement({guaranteed = true, key = 'pop_star_enh'})
           v:set_ability(enhancement, nil, true)
 
           G.E_MANAGER:add_event(Event({
