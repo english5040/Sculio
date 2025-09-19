52d80138a55ac19d7da9d99c43fcee4583c259f6
english5040 <ejwu@andrew.cmu.edu>
2025-09-19 04:51:42 -0400

    Use SMODS probability funcs
    These are required now for mod compatibility:
    probability-modifying effects and triggering on probabilities
    in other mods use these functions

--- jokers/10_anatomy.lua
+++ jokers/10_anatomy.lua
@@ -18,17 +18,22 @@ SMODS.Joker {
   cost = 7,
   blueprint_compat = true,
   loc_vars = function(self, info_queue, card)
-    return { vars = { (G.GAME.probabilities.normal or 1) * 100, card.ability.extra.odds } }
+    local numerator, denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'anatomy')
+    return { vars = { numerator * 100, denominator } }
   end,
   calculate = function(self, card, context)
     if context.cardarea == G.play and context.repetition and not context.repetition_only then
-      card_id = context.other_card:get_id()
+      local card_id = context.other_card:get_id()
 
-      if card_id < 11 and pseudorandom('anatomy') < (G.GAME.probabilities.normal * card_id) / card.ability.extra.odds then
-        return {
-          message = localize('k_again_ex'),
-          repetitions = 1
-        }
+      if card_id < 11 then
+        local n, d = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'anatomy')
+        -- careful: multiply the numerator `n` by card_id after any modifications to `n`.
+        if SMODS.pseudorandom_probability(card, 'anatomy', n * card_id, d, nil, true) then
+          return {
+            message = localize('k_again_ex'),
+            repetitions = 1
+          }
+        end
       end
     end
   end
--- jokers/41_binary.lua
+++ jokers/41_binary.lua
@@ -10,7 +10,8 @@ SMODS.Joker {
   cost = 4,
   blueprint_compat = true,
   loc_vars = function(self, info_queue, card)
-    return { vars = { (G.GAME.probabilities.normal or 1), card.ability.extra.odds, card.ability.extra.chips_gain, card.ability.extra.mult_gain, card.ability.extra.chips, card.ability.extra.mult } }
+    local numerator, denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'binary')
+    return { vars = { numerator, denominator, card.ability.extra.chips_gain, card.ability.extra.mult_gain, card.ability.extra.chips, card.ability.extra.mult } }
   end,
   calculate = function(self, card, context)
     if context.joker_main then
@@ -20,7 +21,8 @@ SMODS.Joker {
       }
     end
 
-    if context.end_of_round and pseudorandom('binary') < G.GAME.probabilities.normal / card.ability.extra.odds and not context.repetition and context.game_over == false then
+    if context.end_of_round and not context.repetition and not context.game_over
+    and SMODS.pseudorandom_probability(card, 'binary', 1, card.ability.extra.odds) then
       card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.chips_gain
       card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.mult_gain
 
