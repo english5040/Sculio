15d2c8ba764e1f52f66a1aa9e30d646838536299
english5040 <ejwu@andrew.cmu.edu>
2025-09-23 18:58:13 -0400

    Enhancement-related jokers should be gated
    These should only show up if you have at least one of that enhancement

--- jokers/29_hammer_and_chisel.lua
+++ jokers/29_hammer_and_chisel.lua
@@ -8,6 +8,7 @@ SMODS.Joker {
   atlas = 'Sculio',
   pos = { x = 0, y = 3 },
   cost = 6,
+  enhancement_gate = 'm_stone',
   blueprint_compat = true,
   loc_vars = function(self, info_queue, card)
     info_queue[#info_queue+1] = G.P_CENTERS.m_stone
